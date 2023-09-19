ad370929a4a8423a5ce24687d39d6cf6e870e7fb
EUNSUN <sunej10@naver.com>
2023-09-20 06:30:25 +0900

    [정은선] - DB 연동하기 : My Batis / JPA - 회원 가입 기능을 위한 API 만들기

---
+++ src/main/java/com/sunny/memo/service/UserService.java
@@ -0,0 +1,30 @@
+package com.sunny.memo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sunny.memo.repository.UserRepository;
+import com.sunny.memo.user.domain.User;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User addUser(
+			String loginId
+			, String password
+			, String name
+			, String email) {
+		
+		User user = User.builder()
+					.loginId(loginId)
+					.password(password)
+					.name(name)
+					.email(email)
+					.build();
+		
+		return userRepository.save(user);
+	}
+}
---
+++ src/main/java/com/sunny/memo/user/UserRestController.java
@@ -0,0 +1,42 @@
+package com.sunny.memo.user;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sunny.memo.service.UserService;
+import com.sunny.memo.user.domain.User;
+
+// API 를 만들기 위한 Controller
+@RestController // @Controller + @ResponseBody
+@RequestMapping("/user")
+public class UserRestController {
+
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/join")
+	public Map<String, String> join(
+			@RequestParam("loginId") String loginId
+			, @RequestParam("password") String password
+			, @RequestParam("name") String name
+			, @RequestParam("email") String email) {
+		
+		User user = userService.addUser(loginId, password, name, email);
+		
+		Map<String, String> resultMap = new HashMap<>();
+		if (user != null) {
+			resultMap.put("result", "success");
+		} else {
+			resultMap.put("result", "fail");
+		}
+		
+		return resultMap;
+		
+	}
+}
