e1baa94900fa6523cc29dab9efbd21449a8b6397
EUNSUN <sunej10@naver.com>
2023-10-18 12:04:36 +0900

    [정은선] 커밋확인

--- src/main/java/com/sunny/memo/config/WebMvcConfig.java
+++ src/main/java/com/sunny/memo/config/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.sunny.memo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.sunny.memo.common.FileManager;
+import com.sunny.memo.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -14,5 +16,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addResourceHandler("/images/**")
 		// 어떤 url 경로로 접근하게 할건지 적어준다
 		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		PermissionInterceptor interceptor = new PermissionInterceptor();
+		
+		registry.addInterceptor(interceptor)
+		.addPathPatterns("/**")  // 모든 페이지
+		.excludePathPatterns("/user/logout" , "/static/**" , "/images/**"); // 예외 주소 입력이 가능
+		
 	}
 }
--- src/main/java/com/sunny/memo/post/service/PostService.java
+++ src/main/java/com/sunny/memo/post/service/PostService.java
@@ -32,6 +32,7 @@ public class PostService {
 	public int updatePost(int postId, String title, String content) {
 		return postRepository.updatePost(postId, title, content);
 	}
+	
 	// 메모 상세 페이지
 	public Post getPost(int id) {
 		// 한 행은 Entity class 여러행은 List
